837f45bf6f572a035ca1a63197faa415566ce574
Brisbie <evanbrisbin@gmail.com>
2024-10-13 14:16:22 -0500

    Initial commit of dotfiles

---
+++ nvim/lua/plugins.lua
@@ -0,0 +1,10 @@
+return{
+    --Dashboard
+    {
+      'goolord/alpha-nvim',
+      config = function ()
+          require'alpha'.setup(require'alpha.themes.dashboard'.config)
+      end
+    };
+  
+  }
